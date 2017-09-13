9058ac23f72050f70f80e0cea6d5557049873e29
Martin Opdam <martin.opdam@avivasolutions.nl>
2017-09-13 19:13:49 +0200

    Initial solution before implementing the neural network algorithm
    Includes an Excel sheet to verify the calculated output, gradients, weights and biases.

---
+++ Source/NeuraNet/Activations/Activation.cs
@@ -0,0 +1,21 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+
+namespace NeuraNet.Activations
+{
+    public abstract class Activation : IActivation
+    {
+        public Vector<double> Transform(Vector<double> values)
+        {
+            return values.Map(Transform, Zeros.Include);
+        }
+
+        public Vector<double> Derivative(Vector<double> values)
+        {
+            return values.Map(Derivative, Zeros.Include);
+        }
+
+        protected abstract double Transform(double value);
+
+        protected abstract double Derivative(double value);
+    }
+}
